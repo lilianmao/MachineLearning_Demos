7d79cb224229af37e6f8aa24767c5720ea46b931
lilin <1911932429@qq.com>
2018-05-09 20:09:58 +0800

    bayes代码提交

--- statistical_learning/bayes/bayes.py
+++ statistical_learning/bayes/bayes.py
@@ -1,6 +1,9 @@
 # -*- coding:utf-8 -*-
 
 from numpy import *
+import re
+import operator
+import feedparser
 
 def loadDataSet():
     postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
@@ -36,25 +39,26 @@ def setOfWords2Vec(vocabList, inputSet):
 def trainNB0(trainMatrix, trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
-    pAbusive = sum(trainCategory) / float(numTrainDocs) # 该文档属于侮辱类的概率是0.5，两边相等。
+    pAbusive = sum(trainCategory) / float(numTrainDocs) # 该文档属于侮辱类的概率是0.5。
     p0Num = ones(numWords); p1Num = ones(numWords)      # 如果是0，显然不合理，最后可能出现log0的情况。
     p0Denom = 2.0; p1Denom = 2.0                        # 这里为啥是2.0看不懂？？？
     for i in range(numTrainDocs):
         if trainCategory[i] == 1:
-            # 当你在C1这个类别的时候，有很多feature，（feature组成的矩阵 / 总和）
-            # 即每个feature在C1类别时的条件概率
+            # 当你在C1这个类别的时候，有很多feature，每一个feature导致了一次label的值
+            # 所以计算公式是：feature组成的矩阵 / 总和，即每个feature在C1类别时的条件概率
             p1Num += trainMatrix[i]
             p1Denom += sum(trainMatrix[i])
         else:
             p0Num += trainMatrix[i]
             p0Denom += sum(trainMatrix[i])
-    p1Vect = log(p1Num / p1Denom)                       # log化，防止数据过于密集
-    p0Vect = log(p0Num / p0Denom)                       # 这里这个总和看不懂？？？
+    p1Vect = log(p1Num / p1Denom)                       # log化，防止数据过于密集，分散数据。
+    p0Vect = log(p0Num / p0Denom)
     return p0Vect, p1Vect, pAbusive
 
 # 划分函数
-# vec2Classify是需要分类的变量
-# 需要分类的分量 * 各个分量的概率的总和，是已经进行过log的，两个log的相加等于相乘，log(p(w|Ci)*p(Ci)) = log(p(w|Ci)) + log(p(Ci))
+# vec2Classify是需要分类的变量，需要分类的分量*p0Vec = 选取出要分类的分量的各自概率，各自概率相乘的积得到的是p(w|Ci)
+# 这里求的是各个分量各自概率的总和。是因为之前已经进行过log，log(p(w1|Ci) * p(w2|Ci)) = log(p(w1|Ci)) + log(p(w2|Ci))
+# 最终：log(p(w|Ci)*p(Ci)) = log(p(w|Ci)) + log(p(Ci))
 def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
     p1 = sum(vec2Classify * p1Vec) + log(pClass1)
     p0 = sum(vec2Classify * p0Vec) + log(1.0-pClass1)
@@ -62,13 +66,6 @@ def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
         return 1
     else:
         return 0
-
-def bagOfWords2VecMN(vocabList, inputSet):
-    returnVec = [0]*len(vocabList)
-    for word in inputSet:
-        if word in vocabList:
-            returnVec[vocabList.index(word)] += 1
-    return returnVec
 
 def testingNB():
     listOPosts, listClasses = loadDataSet()
@@ -88,7 +85,121 @@ def testingNB():
     thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
     print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
 
+# 词袋模型，针对setOfWords2Vec的修改。
+def bagOfWords2VecMN(vocabList, inputSet):
+    returnVec = [0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
+    return returnVec
+
+# 处理文本，小写化，去掉小于2长度的字符串。
+def textParse(bigString):
+    listOfTokens = re.split(r'\W*', bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+    # 统一大小写形式，方便存储。且每个都要大于2。
+
+# 使用朴素贝叶斯进行垃圾邮件的过滤
+def spamTest():
+    # docList：文本list，classList：label的list。
+    docList = [];   classList = []; fullText = [];
+    for i in range(1, 26):
+        wordList = textParse(open('spam/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)   # extend()：在列表末尾一次性追加另一个序列中的多个值。
+        classList.append(1)
+        wordList = textParse(open('ham/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)
+    trainingSet = range(50)             # range(50) 从0到50的list，不包括50。
+    testSet = []                        # 随机选10个放入testSet中。
+    for i in range(10):
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+    trainMat = [];  trainClasses = []   # 构建输入矩阵和输出矩阵。输入数据需要矩阵化。
+    for docIndex in trainingSet:
+        trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))    # train
+    errorCount = 0
+    for docIndex in testSet:
+        wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
+        if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+            errorCount += 1
+            print "classification error",docList[docIndex]
+    print 'the error rate is:', float(errorCount) / len(testSet)
+
+# 计算出现频率最高的30个词
+def calcMostFreq(vocabList, fullText):
+    freqDict = {}
+    for token in vocabList:
+        freqDict[token] = fullText.count(token)
+    sortedFreq = sorted(freqDict.iteritems(), key=operator.itemgetter(1), reverse=True) #迭代器第一个降序排列
+    return sortedFreq[:30]
+
+def localWords(feed1, feed0):
+    docList = [];   classList = [];     fullText = []
+    minLen = min(len(feed1['entries']), feed0['entries'])
+    for i in range(minLen):
+        wordList = textParse(feed1['entries'][i]['summary'])
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)  # NY is class 1
+        wordList = textParse(feed0['entries'][i]['summary'])
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)
+    top30Words = calcMostFreq(vocabList, fullText)
+    for pairW in top30Words:
+        if pairW[0] in vocabList:
+            vocabList.remove(pairW[0])
+    trainingSet = range(2*minLen)
+    testSet = []
+    for i in range(20):
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+    trainMat = [];
+    trainClasses = []
+    for docIndex in trainingSet:  # train the classifier (get probs) trainNB0
+        trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:  # classify the remaining items
+        wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
+        if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+            errorCount += 1
+    print 'the error rate is: ', float(errorCount) / len(testSet)
+    return vocabList, p0V, p1V
+
+def getTopWords(ny,sf):
+    import operator
+    vocabList,p0V,p1V=localWords(ny,sf)
+    topNY=[]; topSF=[]
+    for i in range(len(p0V)):
+        if p0V[i] > -6.0 : topSF.append((vocabList[i],p0V[i]))
+        if p1V[i] > -6.0 : topNY.append((vocabList[i],p1V[i]))
+    sortedSF = sorted(topSF, key=lambda pair: pair[1], reverse=True)
+    print "SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**"
+    for item in sortedSF:
+        print item[0]
+    sortedNY = sorted(topNY, key=lambda pair: pair[1], reverse=True)
+    print "NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**"
+    for item in sortedNY:
+        print item[0]
+
 
 if __name__ == "__main__":
-    testingNB()
+    # testingNB()       # 文档分类
+    # spamTest()        # 判断垃圾邮件
+
+    # TODO 网址没数据，该函数没有成功运行
+    ny = feedparser.parse('http://newyork.craigslist.org/stp/index.rss')
+    sf = feedparser.parse('http://sfbay.craigslist.org/stp/index.rss')
+    localWords(ny, sf)  # 从个人广告中获取区域倾向
 
--- statistical_learning/test.py
+++ statistical_learning/test.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 from numpy import *
+import re
+import feedparser
 
 x = [1, 2, 3]
 x1, x2, x3 = x;
@@ -14,3 +16,15 @@ print martix
 
 printVec = [0]*9
 print printVec, type(printVec)
+
+mySent = 'This book is the best book on Python or M.L. I have ever laid eyes upon.'
+print mySent.split()
+
+regEx = re.compile('\\W*')
+listOfTokens = regEx.split(mySent)
+print listOfTokens
+
+print [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+ny = feedparser.parse('http://newyork.craigslist.org/stp/index.rss')
+print ny, ny['entries'], len(ny['entries'])
